70bad3d771155c021fe91a12a81f35250d8e1beb
Jerbesson Silva <jerbessonc@gmail.com>
2025-03-01 17:26:27 -0300

    Implement project detail view and update project listing template

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'projects'
 
 urlpatterns = [
-    path('', views.projects, name='projects'),
+    path('', views.projects, name='list'),  # Changed from 'projects' to 'list'
+    path('<int:project_id>/', views.project_detail, name='detail'),  # Add detail view URL
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 def projects(request):
     projects = Project.objects.all()
     context = {'projects': projects}
-    return render(request, 'projects/projects.html', context)
+    return render(request, 'projects/home.html')  # Update template name
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    context = {'project': project}
+    return render(request, 'projects/detail.html', context)
